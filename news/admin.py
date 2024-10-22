2ec6e9b47f8e56d273b00e650129d7e3d6eb93a0
samuelsibs <samuelsebastiansibarani@gmail.com>
2024-10-23 00:46:36 +0700

    Specify role admin untuk akses NewsArticle

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import NewsArticle
 
-# Register your models here.
+@admin.register(NewsArticle)
+class NewsArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+
