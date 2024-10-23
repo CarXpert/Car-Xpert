38abb8fa162d0f63f52ac5936add4ab891efb3ed
cleoexcel <cleoxclln@gmail.com>
2024-10-23 14:06:08 +0700

    add main dan urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
+
+# urls.py di app main
 from django.urls import path
-from main.views import show_main
+from . import views
+
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+
+    path('', views.car_list, name='car_list'),  # URL untuk menampilkan daftar mobil di main.html
 ]
