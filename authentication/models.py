bb61af5a7ced589295a0ed9ada267f79e8b70fda
cleoexcel <cleoxclln@gmail.com>
2024-10-23 13:06:58 +0700

    models.py in authentication

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from wishlist.models import wishlist
+from wishlist.models import Wishlist
 from comparecars.models import CompareCarUser
 from bookshowroom.models import Booking
 
@@ -8,6 +8,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=100)
     user_type = models.CharField(max_length=10)
-    wishlist = models.ManyToManyField(wishlist, related_name="wishlist")
+    wishlist = models.ManyToManyField(Wishlist, related_name="wishlist")
     comparecars = models.ManyToManyField(CompareCarUser, related_name="comparecars")
     bookshowroom = models.ManyToManyField(Booking, related_name="bookshowroom")
