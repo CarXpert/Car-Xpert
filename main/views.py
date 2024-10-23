bece938b1176cdd7bafab9a75b48810deb03dd04
khoazmi <khoazmi@gmail.com>
2024-10-23 15:01:29 +0700

    main.html done with car cards

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 # views.py di app main
-from django.shortcuts import render
-from .models import Car
+from django.shortcuts import render, get_object_or_404
+from cars.models import Car
+
 
 def show_main(request):
     cars = Car.objects.all()
@@ -11,5 +12,12 @@ def show_main(request):
         "user": request.user
     }
     return render(request, 'main.html', context)  # Render ke 'main/main.html'
+
+def car_detail(request, car_id):
+    car = Car.objects.filter(pk=car_id)
+    context = {
+        'car': car,
+    }
+    return render(request, 'car_detail.html', context)
 
 
