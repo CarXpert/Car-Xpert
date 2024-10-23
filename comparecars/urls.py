ab2344635a144bbdecb27a45ed37e92065480105
cleoexcel <cleoxclln@gmail.com>
2024-10-23 23:19:32 +0700

    feat: add fitur in comparecars & create header.html

--- comparecars/urls.py
+++ comparecars/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('compare/', views.compare_cars, name='compare_cars'),  
     path('compare/<int:id>/', views.compare_cars_with_id, name='compare_cars_with_id'),  
+    path('get-cars/', views.get_cars, name='get_cars'),  
+    path('list-comparisons/', views.list_comparisons, name='list_comparisons'),
 ]
