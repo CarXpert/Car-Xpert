1faa3e98b355a05773bd29dae84332221dcca4e1
adindawrdhnk <adwrdhnk@gmail.com>
2024-10-27 00:54:04 +0700

    FINISHING WISHLIST FOR REAL (TINGGAL UNIT TEST)

--- main/views.py
+++ main/views.py
@@ -1,4 +1,3 @@
-# views.py di app main
 from django.shortcuts import render, get_object_or_404
 from cars.models import Car
 from wishlist.models import Wishlist
@@ -6,7 +5,7 @@ from news.models import NewsArticle
 
 def show_main(request):
     cars = Car.objects.all()
-    news = NewsArticle.objects.all().order_by('-published_date')[:3]  # Ambil berita terbaru, maksimal 3 berita
+    news = NewsArticle.objects.all().order_by('-published_date')[:3]  # Get the latest 3 news articles
     context = {
         'cars': cars,
         'news': news,  
@@ -18,11 +17,12 @@ def landing_page(request):
     return render(request, 'landing_page.html')
 
 def car_detail(request, car_id):
-    car = Car.objects.get(pk=car_id)
+    car = get_object_or_404(Car, pk=car_id)  # Use get_object_or_404 for better error handling
     is_in_wishlist = Wishlist.objects.filter(user=request.user, car=car).exists() if request.user.is_authenticated else False
     context = { 
         'car': car,
         'showroom': car.showroom,  # The related showroom
         'is_in_wishlist': is_in_wishlist,
+        'user': request.user  # Pass user to the template
     }
     return render(request, 'car_detail.html', context)
