98874c4db0836f319815b6203d628aa8423f1299
adindawrdhnk <adwrdhnk@gmail.com>
2024-10-26 23:36:54 +0700

    Menyelesaikan add and update notes wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import show_wishlist, add_note_to_wishlist_ajax, remove_from_wishlist, add_remove_wishlist, add_or_edit_note, check_wishlist
+from .views import show_wishlist, add_remove_wishlist, remove_from_wishlist, check_wishlist, edit_note
 
 app_name = 'wishlist' 
 
 urlpatterns = [
-    path('add_note/', add_note_to_wishlist_ajax, name='add_note_to_wishlist_ajax'),
     path('wishlist/', show_wishlist, name='wishlist_view'),
     path('add-remove/', add_remove_wishlist, name='add_remove_wishlist'),
     path('remove/', remove_from_wishlist, name='remove_from_wishlist'),
-    path('add_or_edit_note/', add_or_edit_note, name='add_or_edit_note'),
     path('check-wishlist/', check_wishlist, name='check_wishlist'),
+    path('edit-note/<int:pk>/', edit_note, name='edit_note'),  # New URL for editing notes
 ]
