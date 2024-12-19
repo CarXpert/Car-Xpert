8c05f71a5e6ff249451fd60de853a334ae2776ab
cleoexcel <cleoxclln@gmail.com>
2024-12-19 10:50:01 +0700

    add

--- Car_Xpert/settings.py
+++ Car_Xpert/settings.py
@@ -145,4 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
